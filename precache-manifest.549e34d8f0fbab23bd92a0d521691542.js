4929724c11dee8e48d7b80a04e6c2887db606cc4
twoheart <twoheart@vip.qq.com>
2020-08-18 15:44:38 +0800

    deploy

---
+++ precache-manifest.549e34d8f0fbab23bd92a0d521691542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff13cb3ecb00de9afca1c45dcaff744",
+    "url": "/react-accunting-website/index.html"
+  },
+  {
+    "revision": "aa3e6d1f38914afadf76",
+    "url": "/react-accunting-website/static/css/main.b8707586.chunk.css"
+  },
+  {
+    "revision": "f86c7c2c81b66e385b32",
+    "url": "/react-accunting-website/static/js/2.20d27677.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/react-accunting-website/static/js/2.20d27677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3e6d1f38914afadf76",
+    "url": "/react-accunting-website/static/js/main.7d60ef8c.chunk.js"
+  },
+  {
+    "revision": "9c6b76c298cb7504eb61",
+    "url": "/react-accunting-website/static/js/runtime-main.95a78fca.js"
+  }
+]);
